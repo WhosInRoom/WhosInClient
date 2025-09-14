f9ef65acd06e890c87959ff57677a9671d2a70cb
casperjr <hyunjw0515@gmail.com>
2025-09-14 17:41:55 +0900

    [CHORE]: 앱에서 사용할 네비게이션 그래프 작성

--- composeApp/src/commonMain/kotlin/org/whosin/client/core/navigation/WhosInNavGraph.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/core/navigation/WhosInNavGraph.kt
@@ -3,11 +3,56 @@ package org.whosin.client.core.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import org.whosin.client.presentation.auth.LoginScreen
+import org.whosin.client.presentation.home.HomeScreen
+import org.whosin.client.presentation.mypage.MyPageScreen
 
 @Composable
 fun WhosInNavGraph(
-     navController: NavHostController,
-     modifier: Modifier = Modifier
+    navController: NavHostController,
+    modifier: Modifier = Modifier
 ) {
+    NavHost(
+        navController = navController,
+        startDestination = Route.AuthGraph
+    ) {
+        /* 인증,인가 화면들을 위한 별도의 graph */
+        navigation<Route.AuthGraph>(
+            startDestination = Route.Login,
+        ) {
+            composable<Route.Login> {
+                LoginScreen(
+                    modifier = modifier,
+                    onNavigateToHome = {
+                        navController.navigate(Route.Home)
+                    }
+                )
+            }
+        }
 
+        /* 홈 화면 */
+        composable<Route.Home> {
+            HomeScreen(
+                modifier = modifier,
+                onNavigateBack = { navController.navigateUp() },
+                onNavigateToMyPage = {
+                    navController.navigate(Route.MyPage)
+                }
+            )
+        }
+
+        /* 마이페이지 화면 */
+        composable<Route.MyPage> {
+            MyPageScreen(
+                modifier = modifier,
+                onNavigateBack = { navController.navigateUp() },
+                onNavigateToAddClub = {
+
+                }
+            )
+        }
+    }
 }
