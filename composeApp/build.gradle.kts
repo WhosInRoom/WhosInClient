f0797b367132a7376b536a1e7bf9299039e202a0
casperjr <hyunjw0515@gmail.com>
2025-09-14 12:09:50 +0900

    [CHORE]: Koin 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,6 +68,8 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.kotlinx.serialization.json)
+            // Koin
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
