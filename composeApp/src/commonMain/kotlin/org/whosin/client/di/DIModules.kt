113f74b98df3f5e52e9a15481c15207397d159bc
casperjr <hyunjw0515@gmail.com>
2025-09-17 14:58:44 +0900

    [CHORE]: DI 관리를 위한 module들 작성

---
+++ composeApp/src/commonMain/kotlin/org/whosin/client/di/DIModules.kt
@@ -0,0 +1,42 @@
+package org.whosin.client.di
+
+import io.ktor.client.engine.HttpClientEngineFactory
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+import org.whosin.client.core.network.HttpClientFactory
+import org.whosin.client.data.remote.RemoteClubDataSource
+import org.whosin.client.data.remote.RemoteMemberDataSource
+import org.whosin.client.data.repository.ClubRepository
+import org.whosin.client.data.repository.MemberRepository
+import org.whosin.client.presentation.auth.AuthViewModel
+import org.whosin.client.presentation.home.HomeViewModel
+import org.whosin.client.presentation.mypage.MyPageViewModel
+
+fun appModule() = listOf(
+    httpClientModule,
+    dataSourceModule,
+    repositoryModule,
+    viewModelModule
+)
+
+val httpClientModule = module {
+    single{
+        HttpClientFactory.create(get())
+    }
+}
+
+val dataSourceModule = module {
+    single { RemoteMemberDataSource(get()) }
+    single { RemoteClubDataSource(get()) }
+}
+
+val repositoryModule = module {
+    single { MemberRepository(get()) }
+    single { ClubRepository(get()) }
+}
+
+val viewModelModule = module {
+    viewModelOf(::AuthViewModel)
+    viewModelOf(::HomeViewModel)
+    viewModelOf(::MyPageViewModel)
+}
