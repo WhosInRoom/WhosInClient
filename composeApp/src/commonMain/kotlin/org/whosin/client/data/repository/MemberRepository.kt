4d08304dcd4d70a30800bd7b1636841eff810bfa
casperjr <hyunjw0515@gmail.com>
2025-09-17 14:44:53 +0900

    [CHORE]: DataSource를 주입 받을 Repository 생성

---
+++ composeApp/src/commonMain/kotlin/org/whosin/client/data/repository/MemberRepository.kt
@@ -0,0 +1,9 @@
+package org.whosin.client.data.repository
+
+import org.whosin.client.data.remote.RemoteMemberDataSource
+
+class MemberRepository(
+    private val dataSource: RemoteMemberDataSource
+) {
+
+}
