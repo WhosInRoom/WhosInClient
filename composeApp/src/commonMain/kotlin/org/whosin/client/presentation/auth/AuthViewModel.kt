9a85b576097bbcb089d0096ecf304f9532f053ee
casperjr <hyunjw0515@gmail.com>
2025-09-17 14:45:15 +0900

    [CHORE]: 각 화면에서 사용할 ViewModel 생성

---
+++ composeApp/src/commonMain/kotlin/org/whosin/client/presentation/auth/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package org.whosin.client.presentation.auth
+
+import androidx.lifecycle.ViewModel
+import org.whosin.client.data.repository.MemberRepository
+
+class AuthViewModel(
+    private val repository: MemberRepository
+): ViewModel() {
+
+}
