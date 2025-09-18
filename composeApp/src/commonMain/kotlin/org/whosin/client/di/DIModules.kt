c488ca468f45aaa0ca8da25e431fc4db02b4f754
casperjr <hyunjw0515@gmail.com>
2025-09-18 15:49:56 +0900

    [CHORE]: 의존성 추가

--- composeApp/src/commonMain/kotlin/org/whosin/client/di/DIModules.kt
+++ composeApp/src/commonMain/kotlin/org/whosin/client/di/DIModules.kt
@@ -1,15 +1,17 @@
 package org.whosin.client.di
 
-import io.ktor.client.engine.HttpClientEngineFactory
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import org.whosin.client.core.network.HttpClientFactory
+import org.whosin.client.data.remote.DummyDataSource
 import org.whosin.client.data.remote.RemoteClubDataSource
 import org.whosin.client.data.remote.RemoteMemberDataSource
+import org.whosin.client.data.repository.DummyRepository
 import org.whosin.client.data.repository.ClubRepository
 import org.whosin.client.data.repository.MemberRepository
-import org.whosin.client.presentation.auth.AuthViewModel
+import org.whosin.client.presentation.dummy.DummyViewModel
+import org.whosin.client.presentation.auth.LoginViewModel
 import org.whosin.client.presentation.home.HomeViewModel
 import org.whosin.client.presentation.mypage.MyPageViewModel
 
@@ -30,15 +32,19 @@ val httpClientModule = module {
 val dataSourceModule = module {
     single { RemoteMemberDataSource(get()) }
     single { RemoteClubDataSource(get()) }
+    single { DummyDataSource(get()) } // TODO: 이후에 삭제 예정
 }
 
 val repositoryModule = module {
     single { MemberRepository(get()) }
     single { ClubRepository(get()) }
+    single { DummyRepository(get()) } // TODO: 이후에 삭제 예정
 }
 
+// ViewModel을 새로 생성하는 경우에 모듈에 추가하여 사용
 val viewModelModule = module {
-    viewModelOf(::AuthViewModel)
+    viewModelOf(::LoginViewModel)
     viewModelOf(::HomeViewModel)
     viewModelOf(::MyPageViewModel)
+    viewModelOf(::DummyViewModel) // TODO: 이후에 삭제 예정
 }
